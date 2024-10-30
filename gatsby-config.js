09009d632b162234f20cf2fceacc622d1d75cbe9
Emmanuel <ayomikungeeky@gmail.com>
2024-10-30 01:36:04 +0100

    Commit all new changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,8 @@ module.exports = {
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui` // This path is relative to the root of the site.
+        display: `minimal-ui`,
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
   ],
